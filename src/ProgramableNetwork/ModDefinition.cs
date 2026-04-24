67bfb4a13ad476d0ed9cc6fa9cb219f39b9a35dc
Cesar <git@cesar.8shield.net>
2026-04-24 01:21:18 -0300

    Fixing building in release mode

--- src/ProgramableNetwork/ModDefinition.cs
+++ src/ProgramableNetwork/ModDefinition.cs
@@ -26,15 +26,11 @@ namespace ProgramableNetwork
         public override void RegisterPrototypes(ProtoRegistrator registrator) {
             Log.Info($"{nameof(ProgramableNetwork)}: registering prototypes");
             CustomAssetManager.Clear();
-            
+
 			registrator.PrototypesDb.RegisterPhantom(ModuleProto.Phantom);
 
-#if DEBUG
             // Register all prototypes here.
-
-            // Registers all products from this assembly. See ExampleModIds.Products.cs for examples.
             registrator.RegisterAllProducts();
-            //registrator.RegisterData<Terrain>();
 
             // Use data class registration to register other protos such as machines, recipes, etc.
             registrator.RegisterDataWithInterface<IModuleGroup>();
@@ -48,11 +44,9 @@ namespace ProgramableNetwork
 
             // Registers all research from this assembly. See ExampleResearchData.cs for examples.
             registrator.RegisterDataWithInterface<IResearchNodesData>();
-#elif RELEASE
-            // Sanitizer code
+
 			ControllerProto.RegisterPhantom(registrator);
 			DisplayEntityProto.RegisterPhantom(registrator);
-#endif
         }
     }
 
