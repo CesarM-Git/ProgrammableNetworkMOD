b1c20ed27d2e5d7b91dd796c98d1f46f4648281c
Cesar <git@cesar.8shield.net>
2026-04-28 17:12:00 -0300

    Remove log spam
    Commented out unnecessary logs that caused a LOT of lines to be printed

--- src/ProgramableNetwork/Data/Modules/Module.cs
+++ src/ProgramableNetwork/Data/Modules/Module.cs
@@ -169,7 +169,7 @@ namespace ProgramableNetwork
 			StringData = Dict<string, string>.Deserialize(reader);
 			InputModules = Dict<string, ModuleConnector>.Deserialize(reader);
 
-			Log.Info($"[Programable Network] Instance (deserialization): {GetHashCode()}({Id}), version: {loadedVersion}");
+			// Log.Info($"[Programable Network] Instance (deserialization): {GetHashCode()}({Id}), version: {loadedVersion}");
 		}
 
 		[InitAfterLoad(InitPriority.High)]
@@ -177,7 +177,7 @@ namespace ProgramableNetwork
 		public void initContexts(int saveVersion)
 		{
 			Option<ModuleProto> Prototype = Context.ProtosDb.Get<ModuleProto>(new ModuleProto.ID(m_protoId));
-			Log.Info($"[Programable Network] Instance (init): {GetHashCode()}({Id}), version: {loadedVersion}");
+			// Log.Info($"[Programable Network] Instance (init): {GetHashCode()}({Id}), version: {loadedVersion}");
 
 			if (Prototype.HasValue)
 			{
